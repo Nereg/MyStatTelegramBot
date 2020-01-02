2dca88d0a9b83350727e0ab8382f1a9a14dbce8e
nereg <okv23200@gmail.com>
2020-01-02 20:26:46 +0200

    Start working with DB and some small changes

--- main.py
+++ main.py
@@ -2,18 +2,45 @@ import telebot # maintelegram library
 from os import environ # for geting values from parsed env file
 from dotenv import load_dotenv # for parsing .env files
 import API # My MyStat API!
-load_dotenv() 
+import sqlite3# DB !!!
 
+#-------------------------------------------------
+#                   LOADING...
+#-------------------------------------------------
+
+load_dotenv() 
 token = environ.get('token') # get token from .env
 admin_id = environ.get('admin_id') # admin id 
-password = environ.get('password') #default password and username
-APIusername = environ.get('APIusername')
+password = environ.get('password') #default password
+APIusername = environ.get('APIusername') #default username for MyStat
+debug = environ.get('debug') #debug or not
+if debug == 'true': # small little convertion 
+        debug = True
+else:
+        debug = False
+
+#-------------------------------------------------
+#                    INIT
+#-------------------------------------------------
 
 bot = telebot.TeleBot(token) # init bot
+if debug:
+        conn = sqlite3.connect('main.sqlite') #init DB
+else:
+        conn = sqlite3.connect('test.sqlite') #init DB
+cursor = conn.cursor() # make cursor for DB 
+
+#-------------------------------------------------
+#                    TEXT
+#-------------------------------------------------
 
 hello_message = '<b>Приветсвую !</b>\nКороче да я бот\nМой создатель: <b>Олег Кисиль</b>!\nТы можешь к нему обращаться если будут какие либо вопросы.\nТак-же если хочешь узнать как работает этот бот можешь посмотреть исзодный код на <a href="https://github.com/Nereg/MyStatTelegramBot">github</a>\n<b>Удачи!</b>'
 
 help_message = '<b>Нужна помощь?</b>\nНе проблемма!\n Я имею несколько команд и вот их списокю\n/help - выводит эту помощь\nТак-же если думаешь что какая то моя часть работает неправильно или я не отвечаю можешь обратьться к моему\n Кстати вот <a href="tg://user?id={}">ссылочка</a> на него'.format(admin_id)
+
+#-------------------------------------------------
+#                   COMMANDS
+#-------------------------------------------------
 
 @bot.message_handler(commands=['start'])
 def handle_start(message):
@@ -46,7 +73,7 @@ def handle_group_top(message):
 
 # Handles all text messages that contains the commands '/start' or '/help'.
 @bot.message_handler(commands=['stream'])
-def handle_group_top(message):
+def handle_stream_top(message):
         Mymessage = 'Топ потока :\n'
         global password,APIusername
         token = API.getKey(password,APIusername)
@@ -56,4 +83,9 @@ def handle_group_top(message):
         bot.send_message(message.chat.id,Mymessage,True,None,None,'html',False) # yeah so sad just for html
         pass
 
+#-------------------------------------------------
+#                    START!
+#-------------------------------------------------
+
 bot.polling() # START BOT !
+conn.close() # close connection to DB!
--- useful.md
+++
@@ -1,4 +0,0 @@
-How to prase .ENV cofig file : https://pypi.org/project/python-dotenv/
-Telegram API Lib : https://github.com/eternnoir/pyTelegramBotAPI
-Virtual envs : https://docs.python-guide.org/dev/virtualenvs/
-MyStat API (Version of 46d44a5ec61283202fb149e05d38f90bf30a2413 commit) : https://github.com/Nereg/MyStatAPI
